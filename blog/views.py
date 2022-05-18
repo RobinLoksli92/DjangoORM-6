64d7f22d09064ff9f0d67458503db5a759eca05c
RobinLoksli92 <gleb11tiun@mail.ru>
2022-05-19 00:01:47 +0200

    Reduce db queries number.

--- blog/views.py
+++ blog/views.py
@@ -32,7 +32,7 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag(tag) for tag in post.tags.annotate(posts_count=Count('posts'))],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -40,7 +40,7 @@ def serialize_post_optimized(post):
 def serialize_tag(tag):
     return {
         'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
+        'posts_with_tag': tag.posts_count,
     }
 
 
